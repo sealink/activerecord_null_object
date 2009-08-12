a54a21078ab369aae780815e647bc30c9832b93e
Scott Woods <scott@westarete.com>
2009-08-12 11:06:12 -0400

    Begin to fill in specs a bit more.

--- spec/belongs_to_spec.rb
+++ spec/belongs_to_spec.rb
@@ -1,11 +1,45 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
-describe Post do
+describe Comment do
   before(:each) do
-    @post = Post.create!(:body => 'The body.')
+    @comment = Comment.create!(:body => 'The body.')
   end
   
   it "should contain a body" do
-    @post.body.should_not be_empty
+    @comment.body.should_not be_empty
+  end
+
+  describe "without an author" do
+    it "should return a null object for the author" do
+      @comment.author.kind_of?(NullAuthor).should be_true
+    end
+    
+    it "should be nil" do
+      @comment.author.should be_nil
+    end
+    
+    it "should return nil for the author's name" do
+      @comment.author.name.should be_nil
+    end
+  end
+  
+  describe "with an author" do
+    before(:each) do
+      @comment.author = Author.create!(:name => 'James Baker')
+    end
+    
+    it "should return the author object" do
+      @comment.author.kind_of?(Author).should be_true
+      @comment.author.kind_of?(NullAuthor).should_not be_true
+    end
+    
+    it "should return the author's name" do
+      @comment.author.name.should == 'James Baker'
+    end
+    
+    it "should revert to a null object if assigned nil" do
+      @comment.author = nil
+      @comment.author.name.should be_nil
+    end
   end
 end
