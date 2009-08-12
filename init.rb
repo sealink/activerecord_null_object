702b1a76367d22574797e7c04b7aa50d2990d323
Scott Woods <scott@westarete.com>
2009-08-12 08:43:35 -0400

    Rename lib file to match gem name.

--- init.rb
+++ init.rb
@@ -1,4 +1,4 @@
 require 'rubygems'
 require 'activerecord'
 ActiveRecord::ActiveRecordError   # Workaround for loading bug in AR 2.3.2.
-require File.dirname(__FILE__) + '/lib/belongs_to_with_null_object'
+require File.dirname(__FILE__) + '/lib/activerecord_null_object'
