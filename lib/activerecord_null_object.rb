bf7f65235bf17a474cebe9f5463ec9ec47a5a8bb
Scott Woods <scott@westarete.com>
2010-01-09 09:13:22 -0500

    Factor out common code into a separate method.

--- lib/activerecord_null_object.rb
+++ lib/activerecord_null_object.rb
@@ -2,6 +2,35 @@ module ActiveRecord
   module Associations
     module ClassMethods
       
+      # Add null object support to the given accessor method.
+      def add_null_object_support(association_id, options = {}) #:nodoc:
+        # Determine the class of the association.
+        association_class_name = options[:class_name] || association_id.to_s.classify
+        association_class = association_class_name.constantize
+
+        # Determine the null class for this association.
+        null_class_name = "Null" + association_class_name
+        if Object.const_defined?(null_class_name)
+          null_class = Object.const_get(null_class_name.to_sym)
+        else
+          # Define the null class as an ancestor of the association class.
+          null_class = Object.const_set(null_class_name, Class.new(association_class))
+          null_class.class_eval do
+            include Singleton
+            include ActiveRecordNullObject::NullObject
+          end
+        end
+
+        # Modify the "getter" of the relationship to return an
+        # instance of the association's null object instead of returning nil.
+        class_eval do
+          define_method("#{association_id}_with_null_object".to_sym) do
+            send("#{association_id}_without_null_object".to_sym) || null_class.instance
+          end
+          alias_method_chain association_id, :null_object
+        end
+      end
+      
       # Add a boolean :null_object option to belongs_to.
       def belongs_to_with_null_object(association_id, options = {}) #:nodoc:
         # Extract our custom option.
@@ -9,35 +38,7 @@ module ActiveRecord
         # Call the real belongs_to so that the association gets defined.
         belongs_to_without_null_object(association_id, options)
         # Modify the association if need be.
-        if use_null_object
-          
-          # Determine the class of the association.
-          association_class_name = options[:class_name] || association_id.to_s.classify
-          association_class = association_class_name.constantize
-
-          # Determine the null class for this association.
-          null_class_name = "Null" + association_class_name
-          if Object.const_defined?(null_class_name)
-            null_class = Object.const_get(null_class_name.to_sym)
-          else
-            # Define the null class as an ancestor of the association class.
-            null_class = Object.const_set(null_class_name, Class.new(association_class))
-            null_class.class_eval do
-              include Singleton
-              include ActiveRecordNullObject::NullObject
-            end
-          end
-
-          # Modify the "getter" of the belongs_to relationship to return an
-          # instance of the association's null object instead of returning nil.
-          class_eval do
-            define_method("#{association_id}_with_null_object".to_sym) do
-              send("#{association_id}_without_null_object".to_sym) || null_class.instance
-            end
-            alias_method_chain association_id, :null_object
-          end
-          
-        end
+        add_null_object_support(association_id, options) if use_null_object
       end
       alias_method_chain :belongs_to, :null_object
 
@@ -48,35 +49,7 @@ module ActiveRecord
         # Call the real has_one so that the association gets defined.
         has_one_without_null_object(association_id, options)
         # Modify the association if need be.
-        if use_null_object
-          
-          # Determine the class of the association.
-          association_class_name = options[:class_name] || association_id.to_s.classify
-          association_class = association_class_name.constantize
-
-          # Determine the null class for this association.
-          null_class_name = "Null" + association_class_name
-          if Object.const_defined?(null_class_name)
-            null_class = Object.const_get(null_class_name.to_sym)
-          else
-            # Define the null class as an ancestor of the association class.
-            null_class = Object.const_set(null_class_name, Class.new(association_class))
-            null_class.class_eval do
-              include Singleton
-              include ActiveRecordNullObject::NullObject
-            end
-          end
-
-          # Modify the "getter" of the has_one relationship to return an
-          # instance of the association's null object instead of returning nil.
-          class_eval do
-            define_method("#{association_id}_with_null_object".to_sym) do
-              send("#{association_id}_without_null_object".to_sym) || null_class.instance
-            end
-            alias_method_chain association_id, :null_object
-          end
-          
-        end
+        add_null_object_support(association_id, options) if use_null_object
       end
       alias_method_chain :has_one, :null_object
       
